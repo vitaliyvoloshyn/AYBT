fed370777e604f8089e3ffc736387e82a1a8886b
Vitaliy <vitaliy.srt1985@gmail.com>
2024-06-02 22:49:24 +0300

    add models

---
+++ src/models/models.py
@@ -0,0 +1,43 @@
+from typing import List
+
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
+from datetime import date
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+class WorkDay(Base):
+    __tablename__ = 'workdays'
+    id: Mapped[int] = mapped_column(primary_key=True)
+    date: Mapped[date]
+    day_of_week: Mapped[str]
+    description: Mapped[str]
+
+
+class RateType(Base):
+    __tablename__ = 'rate_types'
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str]
+    rates: Mapped[List['Rate']] = relationship(back_populates='rate_type')
+
+
+class Rate(Base):
+    __tablename__ = 'rates'
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str]
+    rate_type_id: Mapped[int] = mapped_column(ForeignKey('rate_types.id'))
+    rate_type: Mapped['RateType'] = relationship(back_populates='rates')
+    rate_values: Mapped[List['RateValue']] = relationship(back_populates='rate')
+
+
+class RateValue(Base):
+    __tablename__ = 'rate_values'
+    id: Mapped[int] = mapped_column(primary_key=True)
+    value: Mapped[int]
+    start_date: Mapped[date]
+    end_date: Mapped[date] = mapped_column(nullable=True)
+    rate_id: Mapped[int] = mapped_column(ForeignKey('rates.id'))
+    rate: Mapped['Rate'] = relationship(back_populates='rate_values')
