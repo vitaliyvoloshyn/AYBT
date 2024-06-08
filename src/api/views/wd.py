2bda0229842652133dd8397784bc453e96e85434
Vitaliy <vitaliy.srt1985@gmail.com>
2024-06-08 19:27:04 +0300

    add controllers to wd view - get_all_days, delete_wd, update_wd

--- src/api/views/wd.py
+++ src/api/views/wd.py
@@ -1,6 +1,6 @@
-from typing import Annotated, List
+from typing import List
 
-from fastapi import APIRouter, Form
+from fastapi import APIRouter
 
 from src.repositories.SQLRepository import WorDayRepository
 from src.schemas.schemas import WorkDayDTO, WorkDayAddDTO
@@ -15,6 +15,24 @@ def get_all_days():
     return wd_service.get_all()
 
 
+@wd_router.get('/{pk}', response_model=List[WorkDayDTO])
+def get_all_days(pk: int):
+    return wd_service.get_obj(id=pk)
+
+
 @wd_router.post('/add')
 def add_wd(wd: WorkDayAddDTO):
     return wd_service.add(wd)
+
+
+@wd_router.delete('/delete')
+def delete_wd(pk: int):
+    wd_service.delete(pk)
+    return {'status': 'OK'}
+
+
+@wd_router.patch('/{pk}')
+def update_wd(pk: int, wd: WorkDayAddDTO):
+    data_ = wd.dict()
+    wd_service.update(pk, data_)
+    return {'status': 'OK'}
