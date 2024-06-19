9a264909b73b49ad9798f0fb587c224b80ba8f4e
Vitaliy <vitaliy.srt1985@gmail.com>
2024-06-19 20:34:58 +0300

    додані views для виводу зарплати

--- src/api/views/reports.py
+++ src/api/views/reports.py
@@ -19,3 +19,14 @@ def get_fact_work_days(month: int, year: int):
 @report_router.get('/get_wage')
 def get_wage_per_month(month: int, year: int, fact_wage: bool = True):
     return report_service.get_wage_per_month(month, year, fact_wage)
+
+
+@report_router.get('/payments_per_month')
+def get_fact_payments_per_month(month: int, year: int):
+    """dgdfgdfgd"""
+    return report_service.get_fact_payments_per_month(month, year)
+
+
+@report_router.get('/payments_billing_date')
+def get_payments_per_month_billing(month: int, year: int):
+    return report_service.get_fact_payments_per_month_billing(month, year)
