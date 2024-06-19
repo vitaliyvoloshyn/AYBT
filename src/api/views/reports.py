5062c1812f687ade6d3a71a2490dda8965d582a2
Vitaliy <vitaliy.srt1985@gmail.com>
2024-06-19 19:30:11 +0300

    add get_wage_per_month view

--- src/api/views/reports.py
+++ src/api/views/reports.py
@@ -6,9 +6,11 @@ from src.services import IService
 report_router = APIRouter(prefix='/reports', tags=['Reports'])
 report_service = IService()
 
+
 @report_router.get('/get_plan_work_days')
 def get_plan_work_days(month: int, year: int):
     return report_service.get_plan_wd_per_month(month, year)
+
 
 @report_router.get('/get_fact_work_days')
 def get_fact_work_days(month: int, year: int):
@@ -16,5 +18,5 @@ def get_fact_work_days(month: int, year: int):
 
 
 @report_router.get('/get_wage')
-def get_wage_per_month(month: int, year: int):
-    return report_service.get_wage_per_month(month, year)
+def get_wage_per_month(month: int, year: int, fact_wage: bool = True):
+    return report_service.get_wage_per_month(month, year, fact_wage)
